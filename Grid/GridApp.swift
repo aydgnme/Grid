697079a1d7aa122641a72a8ac4b49efc794978f3
aydgnme <mertaydogn0@gmail.com>
2025-11-25 21:47:58 +0200

    Initial Commit

---
+++ Grid/GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridApp.swift
+//  Grid
+//
+//  Created by Mert Aydogan on 25.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
